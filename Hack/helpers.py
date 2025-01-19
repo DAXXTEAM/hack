99babb7bdb9f8d1e5819191588444def2e210afd
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2025-01-19 05:31:57 -0800

    Update helpers.py

--- Hack/helpers.py
+++ Hack/helpers.py
@@ -178,3 +178,49 @@ def on_callback(data=None):
         bot.add_event_handler(wrap, CallbackQuery(data=data))
 
     return dec
+
+
+# https://github.com/TeamUltroid/Ultroid/blob/main/pyUltroid/startup/connections.py
+
+_PYRO_FORM = {351: ">B?256sI?", 356: ">B?256sQ?", 362: ">BI?256sQ?"}
+
+DC_IPV4 = {
+    1: "149.154.175.53",
+    2: "149.154.167.51",
+    3: "149.154.175.100",
+    4: "149.154.167.91",
+    5: "91.108.56.130",
+}
+
+
+def validate_session(session):
+    # Telethon Session
+    if session.startswith(CURRENT_VERSION):
+        if len(session.strip()) != 353:
+            return False
+        return StringSession(session)
+
+    # Pyrogram Session
+    elif len(session) in _PYRO_FORM.keys():
+        if len(session) in [351, 356]:
+            dc_id, _, auth_key, _, _ = struct.unpack(
+                _PYRO_FORM[len(session)],
+                base64.urlsafe_b64decode(session + "=" *
+                                         (-len(session) % 4)),
+            )
+        else:
+            dc_id, _, _, auth_key, _, _ = struct.unpack(
+                _PYRO_FORM[len(session)],
+                base64.urlsafe_b64decode(session + "=" *
+                                         (-len(session) % 4)),
+            )
+        return StringSession(CURRENT_VERSION + base64.urlsafe_b64encode(
+            struct.pack(
+                _STRUCT_PREFORMAT.format(4),
+                dc_id,
+                ipaddress.ip_address(DC_IPV4[dc_id]).packed,
+                443,
+                auth_key,
+            )).decode("ascii"))
+    else:
+        return False
