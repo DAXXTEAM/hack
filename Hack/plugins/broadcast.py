daa5e6b56634b30fe44da7df6a3b7aec3150f345
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2025-01-19 05:25:08 -0800

    Update broadcast.py

--- Hack/plugins/broadcast.py
+++ Hack/plugins/broadcast.py
@@ -9,19 +9,19 @@ from telethon import events, errors
 @bot.on(events.NewMessage(pattern=r"/broadcast\s*([\s\S]*)?"))
 async def broadcast(event):
     if not DB:
-        await event.reply('Add Mongo Url First')
+        await event.reply('**Add Mongo URL First**')
         return
     if not (event.sender_id in env.SUDOERS):
         return
     text = event.pattern_match.group(1)
     reply = await event.get_reply_message()
     if not (text or reply):
-        return await event.reply('Please Give A Text Or Reply To a Message')
+        return await event.reply('**Please Give A Text Or Reply To a Message**')
     sent = 0
     ids = await DB.get_users()
 
     msg = await event.reply(
-        "**Processing....\nPlease Dont Delete The Replied Message**"
+        "**Processing....\nPlease Don't Delete The Replied Message**"
     ) if reply else await event.reply('**Processing....**')
     for user in ids:
         try:
@@ -34,7 +34,7 @@ async def broadcast(event):
         except Exception:
             continue
 
-    result = f"**Broadcasted In {len(ids)} Chats **"
+    result = f"**Broadcasted In {len(ids)} Chats**"
     try:
         await msg.edit(result)
     except:
